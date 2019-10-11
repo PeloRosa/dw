fc9cd9562a9fb43197aa790da490523d0e190daf
pelo_rosa <pelo_rosa@163.com>
2019-10-11 23:19:39 +0800

    登陆页面的样式更新

--- gulpfile.js
+++ gulpfile.js
@@ -3,45 +3,45 @@ var gulp = require("gulp");
 var sass = require("gulp-sass");
 
 gulp.task("copy-html",function(){
-    return gulp.src("index.html").pipe(gulp.dest("F:\\publishspace\\DW_publish"));
+    return gulp.src("index.html").pipe(gulp.dest("D:\\phpStudy\\WWW\\DW"));
 });
 
 gulp.task("copyAllFile",function(){
-    return gulp.src("*/**/*").pipe(gulp.dest("F:\\publishspace\\DW_publish"));
+    return gulp.src(["*/**/*","!scss/**/*","*.html"]).pipe(gulp.dest("D:\\phpStudy\\WWW\\DW"));
 });
 
 // gulp.task("copy-data",function(){
-//     return gulp.src(["json/*.json","!json/filter.json"]).pipe(gulp.dest("F:\\publishspace\\DW_publish\\"));
+//     return gulp.src(["json/*.json","!json/filter.json"]).pipe(gulp.dest("D:\\phpStudy\\WWW\\DW\\\\"));
 // });
 
 //自动生成sass编译后文件
-gulp.task("sass",function(){
-    return gulp.src("stylesheet/**/*.scss")
-    .pipe(sass())
-    .pipe(gulp.dest("F:\\publishspace\\DW_publish\\css"));
-});
+// gulp.task("sass",function(){
+//     return gulp.src("stylesheet/**/*.scss")
+//     .pipe(sass())
+//     .pipe(gulp.dest("D:\\phpStudy\\WWW\\DW\\css"));
+// });
 
 gulp.task("watchall",async ()=>{
     //定义一个监听器，监听文件是否有改变，如果有改变，则自动拷贝。
     gulp.watch("*.html",async ()=>{
         gulp.src("*.html")
-        .pipe(gulp.dest("F:\\publishspace\\DW_publish"));
+        .pipe(gulp.dest("D:\\phpStudy\\WWW\\DW"));
     });
 
     gulp.watch("js/*.js",async ()=>{
         gulp.src("js/*.js")
-        .pipe(gulp.dest("F:\\publishspace\\DW_publish\\js"));
+        .pipe(gulp.dest("D:\\phpStudy\\WWW\\DW\\js"));
     });
 
     gulp.watch("css/*.css",async ()=>{
         gulp.src("css/*.css")
-        .pipe(gulp.dest("F:\\publishspace\\DW_publish\\css"));
+        .pipe(gulp.dest("D:\\phpStudy\\WWW\\DW\\css"));
     });
 
-    gulp.watch("stylesheet/**/*.scss",async ()=>{
-        gulp.src("stylesheet/**/*.scss")
+    gulp.watch("scss/**/*.scss",async ()=>{
+        gulp.src("scss/**/*.scss")
         .pipe(sass())
-        .pipe(gulp.dest("F:\\publishspace\\DW_publish\\css"));
+        .pipe(gulp.dest("F:\\workspace\\Git_Project\\dw\\css"));
     });
 
 });
